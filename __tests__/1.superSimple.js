6760a3c4a8888b21df5ed7ebd75bcd980da9a37c
HenryYenHa <henryha@live.ca>
2023-11-30 11:34:44 -0700

    Class notes

--- __tests__/1.superSimple.js
+++ __tests__/1.superSimple.js
@@ -1,10 +1,10 @@
-describe('simple tests', ()=>{
-    describe('first tests', ()=>{
-        it('should pass', ()=>{
-            expect(true).toBeTruthy()
-        })
-        // it('should fail', ()=>{
-        //     expect(true).not.toBeTruthy()
-        // })
-    })
-})
+describe("simple tests", () => {
+  describe("first tests", () => {
+    it("should pass", () => {
+      expect(true).toBeTruthy();
+    });
+    test("should fail", () => {
+      expect(true).not.toBeTruthy();
+    });
+  });
+});
